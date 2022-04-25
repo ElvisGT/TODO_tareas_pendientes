447bd2adb7367b8ee9c60bbd404f2e67a7caf93b
Elvis <elvisgiltorres@gmail.com>
2022-04-25 19:21:43 -0400

    Estilando la aplicacion con las clases de Boostrap

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -6,7 +6,7 @@ const Todo = () => {
     const {todo,addTodo,removeTodo} = useTODO();
     const addInput = useRef(null);
     const searchInput = useRef(null); 
-    const [searchList,setSearchList] = useState([]);
+    const [searchList,setSearchList] = useState('');
 
 
     const handleAddTODO = (todo ) => {
@@ -32,10 +32,19 @@ const Todo = () => {
         <div className="container">
             <div className="box">
                 <label htmlFor="search" className="form-label">Has agregado {todo.todo.length} TODOs a la lista</label>
-                <input type="text" className="form-control" id="search" placeholder="Buscar TODO" onChange={handleChange} ref={searchInput}></input>
                 
-                <h1 className="box-title">Crea tu primer TODO!</h1>
+                {todo.todo.length > 0 &&
+                    <input type="text" className="form-control" id="search" placeholder="Buscar TODO" onChange={handleChange} ref={searchInput} autoComplete="off" maxLength="30"></input>
+                }   
                 
+                {todo.todo.length === 0 &&
+                    <h1 className="h1">Crea tu primer TODO!</h1>
+                }
+
+                {showTODOS.length === 0 && todo.todo.length > 0  &&
+                    <h1 className="lead">Sin resultados...</h1>
+                }   
+
                 <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                     <div className="modal-dialog">
                         <div className="modal-content">
@@ -44,7 +53,7 @@ const Todo = () => {
                             <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                         </div>
                         <div className="modal-body">
-                            <input type="text" className="form-control" id="search" placeholder="Agregar" ref={addInput}></input>
+                            <input type="text" className="form-control" id="search" placeholder="Agregar" ref={addInput} maxLength="30"></input>
                         </div>
                         <div className="modal-footer">
                             <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
@@ -55,21 +64,20 @@ const Todo = () => {
                     </div>
                 
 
+               
+
+                {todo.todo.length > 0 &&
+                        <ol className="list-group list-group-numbered">
+                            {
+                                showTODOS.map(item => (
+                                <li className="list-group-item" key={item}>{item} <button type="button" className="btn-close" aria-label="Close" onClick={() => handleRemoveTODO(item)}></button></li>
+                                ))
+                            }
+                        </ol>
+                }
                 <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
                 Agregar TODO a la lista
                 </button>
-
-
-                <div className="box-list">
-                    <ul>
-                        {
-                            showTODOS.map(item => (
-                            <li key={item}>{item} <button type="button" className="btn-close" aria-label="Close" onClick={() => handleRemoveTODO(item)}></button></li>
-                            ))
-                        }
-                    </ul>
-                   
-                </div>
             </div>
         </div>
     )
